df226c16c3249eeb36b692d9cfb5aaedf12e1fc6
Alexander Benedict <abenedi1@cs.binghamton.edu>
2017-02-21 14:40:18 -0500

    Functionally working

---
+++ lab05/btree.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+struct tnode {
+   struct tnode * left;
+   struct tnode * right;
+   int value;
+};
+
+struct tnode * makeNode(int value);
+void freeTree(struct tnode * t);
+
+void insertVal(struct tnode * t, int value);
+void prLVR(struct tnode * t);
+void prRVL(struct tnode * t);
+void printTree(struct tnode * t, char * prefix);
+
+int i;
+
+int main(int argc, char **argv) {
+   int i;
+
+   if (argc<2) {
+		printf("You must specify at least one argument to %s\n",argv[0]);
+		return 1;
+	}
+	struct tnode * root=makeNode(atoi(argv[1]));
+   for(i=2; i<argc; i++) {
+      int val=atoi(argv[i]);
+      insertVal(root,val);
+   }
+   printf("In LVR order: ");
+   prLVR(root);
+   printf("\nIn RVL order: ");
+   prRVL(root);
+   printf("\nGraphic Representation...\n");
+   printTree(root,"");
+   freeTree(root);
+   return 0;
+}
+
+struct tnode * makeNode(int value) {
+   struct tnode * t = malloc(sizeof(struct tnode));
+   t->value=value;
+   t->left=NULL;
+   t->right=NULL;
+   return t;
+}
+
+void freeTree(struct tnode * t) {
+   assert(t);
+   if (t->left) freeTree(t->left);
+   if (t->right) freeTree(t->right);
+   free(t);
+}
+
+void insertVal(struct tnode * t, int value) {
+	assert(t);
+	if(value <= t->value ){
+		if(t->left == NULL){
+			t->left = makeNode(value);
+		}
+		else{
+			insertVal(t->left,value);
+		}
+	}
+	else if(value > t->value){
+		if(t->right == NULL){
+			t->right = makeNode(value);
+		}
+		else{
+			insertVal(t->right,value);
+		}
+	}
+}
+
+void prLVR(struct tnode * t) {
+	assert(t);
+	if(t->left != NULL){
+		prLVR(t->left);
+	}
+	printf("%d ",t->value);
+	if(t->right != NULL){
+		prLVR(t->right);
+	}
+}
+
+
+void prRVL(struct tnode * t) {
+	assert(t);
+	if(t->right != NULL){
+		prRVL(t->right);
+	}
+	printf("%d ",t->value);
+	if(t->left != NULL){
+		prRVL(t->left);
+	}
+}
+
+void printTree(struct tnode * t, char * prefix) {
+	assert(t);
+	if(t->right != NULL){
+		int len = strlen(prefix);
+		char str[len + 4];
+		strcpy(str,prefix);
+		str[len] = ' ';
+		str[len+1] = ' ';
+		str[len+2] = '|';
+		str[len+3] = '\0';
+		printTree(t->right,str);
+	}
+	if(t->left != NULL || t->right != NULL){
+		printf("%s%d%c\n",prefix,t->value,'+');
+	}
+	else{
+		printf("%s%d \n",prefix,t->value);
+	}
+	if(t->left != NULL){
+		int len = strlen(prefix);
+		char str[len + 4];
+		strcpy(str,prefix);
+		str[len] = ' ';
+		str[len+1] = ' ';
+		str[len+2] = '|';
+		str[len+3] = '\0';
+		printTree(t->left,str);
+	}
+}
+
