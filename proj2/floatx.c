031b5b0517287b39c7f6634fd4da40a2eb775209
Alexander Benedict <abenedi1@binghamton.edu>
2017-03-14 20:55:51 -0400

    Have access to parts of double

--- proj2/floatx.c
+++ proj2/floatx.c
@@ -1,12 +1,35 @@
 #include "floatx.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 /*--------------------------------------------------------------------------------
 	Return floatx representation (as defined by *def) which
 	best approximates value
 -------------------------------------------------------------------------------- */
+
 floatx doubleToFloatx(const floatxDef *def, double value) {
+    union {
+		double d;
+		unsigned long i;
+		unsigned char c[8];
+	} representations;
+	printf("\n\n");
+	representations.d = value;
+	bool sign = (representations.i&0x8000000000000000);
+	printf("Sign: %d \n",sign);
+	unsigned long EXP=(representations.i & 0x7FF0000000000000)>>52;
+	//EXP-1023 because thats half of the bits reserved for exp
+	printf("Exponent: %lu \n", (EXP-1023));
+	unsigned long FRAC=(representations.i & 0xFFFFFFFFFFFFF);
+	printf("Fraction: %lu \n", FRAC);
+	bool twoFrac = (representations.i & 0x8000000000000);
+	printf("1/2: %d \n", twoFrac);
+	bool fourFrac= (representations.i & 0x4000000000000);
+	printf("1/4: %d \n", fourFrac);
+    return 0;
+
 	/* Put your code here */
-	return 0;
 }
 
 /** Return C double with value which best approximates that of floatx fx
