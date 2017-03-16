566d341c4e8474f6e8bb7c2826c9052c44c70278
Alexander Benedict <abenedi1@binghamton.edu>
2017-03-15 22:34:25 -0400

    sign and exponent working

--- proj2/floatx.c
+++ proj2/floatx.c
@@ -16,18 +16,48 @@ floatx doubleToFloatx(const floatxDef *def, double value) {
 	} representations;
 	printf("\n\n");
 	representations.d = value;
-	bool sign = (representations.i&0x8000000000000000);
-	printf("Sign: %d \n",sign);
 	unsigned long EXP=(representations.i & 0x7FF0000000000000)>>52;
 	//EXP-1023 because thats half of the bits reserved for exp
-	printf("Exponent: %lu \n", (EXP-1023));
+	printf("Exponent: %lu \n", (EXP));
 	unsigned long FRAC=(representations.i & 0xFFFFFFFFFFFFF);
 	printf("Fraction: %lu \n", FRAC);
 	bool twoFrac = (representations.i & 0x8000000000000);
 	printf("1/2: %d \n", twoFrac);
 	bool fourFrac= (representations.i & 0x4000000000000);
 	printf("1/4: %d \n", fourFrac);
-    return 0;
+
+	int i;
+	unsigned long leftBit = 1;
+	//finds furthes left value 
+	for(i = 0; i < (*def).totBits -1; i++){
+		leftBit*=2;
+	}
+
+	printf("totBits: %d \n",(*def).totBits);
+	printf("left bit %lu \n",leftBit);
+	
+
+	unsigned long oneBitDouble = 0x8000000000000000;
+	bool sign = (representations.i&oneBitDouble);
+	printf("Sign: %d \n",sign);
+	//zero out ret(return value)
+	floatx ret = 0;
+	//assign sign bit
+	if(sign)ret +=leftBit;
+	//assigning exp bits
+	unsigned long bitMaskExp =1;
+	for(i=0; i< (*def).expBits; i++){
+		bitMaskExp*=2;
+	}
+	bitMaskExp/=2;
+	bitMaskExp--;
+	unsigned long exp = EXP -1023;
+	bitMaskExp+=exp;
+
+	ret = ret|(bitMaskExp<<((*def).totBits-(*def).expBits)-1);
+
+
+    return ret;
 
 	/* Put your code here */
 }
